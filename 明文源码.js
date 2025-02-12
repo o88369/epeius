b148b5a4b3f2779e829256c5219667f5d41fe9e5
o88369 <90207814+o88369@users.noreply.github.com>
2025-02-12 16:36:55 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from "cloudflare:sockets";
 
-let password = '';
+let password = 'AKKA';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
