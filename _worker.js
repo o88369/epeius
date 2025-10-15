ae3eb10ca835572bb03f3eb38822ac0630034081
o88369 <90207814+o88369@users.noreply.github.com>
2025-10-16 01:09:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from "cloudflare:sockets";
 
-let password = '';
+let password = '9K';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
